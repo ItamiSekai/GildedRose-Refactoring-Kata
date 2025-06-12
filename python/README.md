20b51991fa9ac6d6561f6e6c2be5bc319b55883b
ItamiSekai <105084527+ItamiSekai@users.noreply.github.com>
2025-06-12 11:00:15 -0300

    Finalização

--- python/README.md
+++ python/README.md
@@ -1,29 +1,52 @@
-# Gilded Rose starting position in Python
+# Mini Relatório - Gilded Rose - Refatoração
+## Nome: Gabriel Gomes Nogueira (GU3055361)    Nome: Samuel Vitalino Leite (GU3056457)
+## Disciplina: Engenharia de Software
 
-For exercise instructions see [top level README](../README.md)
+## Descrição da atividade
 
-Suggestion: create a python virtual environment for this project. See the [documentation](https://docs.python.org/3/library/venv.html)
+Este projeto consiste em realizar a refatoração do código original do sistema Gilded Rose, que gerencia o estoque de uma loja com diversos tipos de itens. O código original apresentava uma função update_quality com diversas condições aninhadas e de difícil manutenção. O objetivo foi tornar o código mais claro, modularizado e fácil de entender, além de implementar a regra adicional para os itens Conjured.
 
-## Run the unit tests from the Command-Line
+## Processo de refatoração
+- As principais mudanças realizadas foram:
 
-```
-python test_gilded_rose.py
-```
+Separação do comportamento por tipo de item dentro do método update_item_quality, alterando o item conforme o necessário, retirando todos aqueles "if" desnecessários. Deixando mais simples e legível, dividindo em pequenos métodos. Como estes abaixo.
 
-## Run the TextTest fixture from the Command-Line
+- Criação de métodos auxiliares para cada comportamento específico:
 
-For e.g. 10 days:
+update_aged_brie
 
-```
-python texttest_fixture.py 10
-```
+update_backstage_pass
 
-You should make sure the command shown above works when you execute it in a terminal before trying to use TextTest (see below).
+update_conjured_item
+
+update_normal_item
+
+- Criação dos métodos utilitários:
+
+increase_quality
+
+decrease_quality
+
+- Implementação da regra para Conjured:
+
+Itens cujo nome começa com "Conjured" passam a perder qualidade duas vezes mais rápido que os itens normais. Assim como havia sido descrito na atividade.
+
+- Preservação das regras gerais do sistema:
+
+Qualidade mínima = 0
+
+Qualidade máxima = 50 (exceto Sulfuras, que é fixo)
 
 
-## Run the TextTest approval test that comes with this project
+## Dificuldades encontradas
 
-There are instructions in the [TextTest Readme](../texttests/README.md) for setting up TextTest. You will need to specify the Python executable and interpreter in [config.gr](../texttests/config.gr). Uncomment these lines:
+- Manter o código organizado e fácil de entender, mesmo com a necessidade de múltiplas regras. O que gerou uma certa confusão no começo, por ter que dividir em algumas funções, mas que de forma geral fica mais intuitiva e mais fácil de entender.
 
-    executable:${TEXTTEST_HOME}/python/texttest_fixture.py
-    interpreter:python
+- Ler o código e entender antes de refatorar. Todos aqueles "if" acabaram confundindo bastante no geral, principalmente para entender a lógica geral. No fim acabou sendo mais simples apagar tudo e fazer novamente.
+
+## Lições aprendidas
+- A importância de separar claramente as responsabilidades no código, facilitando manutenção futura. Deixando que cada método tenha uma função principal, sem que faça várias coisas ao mesmo tempo e deixe o código ainda maior.
+
+- Métodos auxiliares. Eles ajudam a manter o código mais simples e legível, sem falar coma sua reutilização. Ao invés de refazer sempre a mesma parte de código é apenas chamar a função, como increase_quality, é fácil entender o que vai acontencer após eu usar ele. E caso precise usar mais de uma vez é apenas usar a função novamente.
+
+- A importância de garantir que todos os casos sejam bem tratados. Respeitando todas as regras implementadas, fazerem funcionar da maneira correta.
--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 class GildedRose(object):
 
     def __init__(self, items):
@@ -7,33 +5,60 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                continue  # Sulfuras não muda
+
+            self.update_sell_in(item)
+            self.update_item_quality(item)
+
+    def update_sell_in(self, item):
+        item.sell_in -= 1
+
+    def update_item_quality(self, item):
+        if item.name == "Aged Brie":
+            self.update_aged_brie(item)
+        elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+            self.update_backstage_pass(item)
+        elif item.name.startswith("Conjured"):
+            self.update_conjured_item(item)
+        else:
+            self.update_normal_item(item)
+
+    def update_aged_brie(self, item):
+        self.increase_quality(item)
+        if item.sell_in < 0:
+            self.increase_quality(item)
+
+    def update_backstage_pass(self, item):
+        if item.sell_in < 0:
+            item.quality = 0
+            return
+
+        self.increase_quality(item)
+        if item.sell_in < 10:
+            self.increase_quality(item)
+        if item.sell_in < 5:
+            self.increase_quality(item)
+
+    def update_conjured_item(self, item):
+        self.decrease_quality(item)
+        self.decrease_quality(item)
+        if item.sell_in < 0:
+            self.decrease_quality(item)
+            self.decrease_quality(item)
+
+    def update_normal_item(self, item):
+        self.decrease_quality(item)
+        if item.sell_in < 0:
+            self.decrease_quality(item)
+
+    def increase_quality(self, item):
+        if item.quality < 50:
+            item.quality += 1
+
+    def decrease_quality(self, item):
+        if item.quality > 0:
+            item.quality -= 1
 
 
 class Item:
@@ -44,3 +69,19 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+    
+
+
+items = [
+    Item(name="Aged Brie", sell_in=10, quality=25),
+    Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+    Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+    Item(name="Conjured Mana Cake", sell_in=3, quality=6),
+    Item(name="Normal Item", sell_in=5, quality=7)
+]
+
+gilded_rose = GildedRose(items)
+gilded_rose.update_quality()
+
+for item in items:
+    print(item)
